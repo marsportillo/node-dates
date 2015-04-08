e03d28ae3942da18902286399130254891178d85
Vincent Peybernes <vpeybernes.pro@gmail.com>
2015-04-08 16:40:51 +0200

    Add spanish support

---
+++ CHANGELOGS.md
@@ -0,0 +1,4 @@
+# 1.1.0
+ - Add spanish support.
+
+# 1.0.0
---
+++ libs/i18n/es.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Techniv on 25/08/2014.
+ */
+
+module.exports = {
+    days: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    days3: ["Do.", "Lu.", "Ma.", "Mi.", "Ju.", "Vi.", "Sá."],
+    month: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    month3: ["Ene", "Feb", "Mar", "Abr", "May", "Jun",
+        "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]
+};
