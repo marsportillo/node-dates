089a3998728fa517a5130f1592f8e1bf7e45ca76
Marco Sportillo <msportillo@gmail.com>
2020-08-17 11:43:50 +0200

    Support for Italian language
    Create it.js into i18n folder for italian language.

---
+++ libs/i18n/it.js
@@ -0,0 +1,12 @@
+/**
+ * Created by marsportillo on 17/08/2020.
+ */
+
+module.exports = {
+    days: ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"],
+    days3: ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"],
+    month: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
+        "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+    month3: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu",
+        "Lug", "Ago", "Set", "Ott", "Nov", "Dic"]
+};
